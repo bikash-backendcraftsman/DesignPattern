5342cdbef0325568a08d50fa6209a31eeefa31b5
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2025-10-07 21:24:52 +0530

    (feat) : logic implemented for [DCL] Double CXhecking Lock singleton Design Pattern

---
+++ SingletonPattern/DesignPatternConcept/src/main/java/com/technostack/conceptImpl/DoubleCheckSingleton.java
@@ -0,0 +1,20 @@
+package com.technostack.conceptImpl;
+
+public class DoubleCheckSingleton {
+    private static volatile DoubleCheckSingleton instance;
+
+    private DoubleCheckSingleton(){
+
+    }
+
+    public static DoubleCheckSingleton getInstance(){
+        if(instance == null){
+            synchronized (DoubleCheckSingleton.class){
+                if(instance == null){
+                    instance = new DoubleCheckSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
