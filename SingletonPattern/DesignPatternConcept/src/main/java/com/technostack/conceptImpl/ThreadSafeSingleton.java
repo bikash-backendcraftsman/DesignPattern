12f2a52532038f5f8a1c58faeab0dff3f952a685
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2025-10-07 21:00:11 +0530

    (doc) : commet added in ThreadSafeSingleton

--- SingletonPattern/DesignPatternConcept/src/main/java/com/technostack/conceptImpl/ThreadSafeSingleton.java
+++ SingletonPattern/DesignPatternConcept/src/main/java/com/technostack/conceptImpl/ThreadSafeSingleton.java
@@ -3,16 +3,15 @@ package com.technostack.conceptImpl;
 public class ThreadSafeSingleton {
     //The single instance , initially null
     private static ThreadSafeSingleton instance;
-
     //Constructor is private , so that we prevent more object creation/instantiation
     private ThreadSafeSingleton(){
 
     }
-
     /**
      * public method to get the instance, with synchronized keyword
      * @return instance
      */
+
     public static synchronized ThreadSafeSingleton getInstance(){
         //check if instance is null
         if(instance == null){
