717d33d0e8d9147e572980e4a86b2f4f7bf5311c
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2025-10-09 21:49:43 +0530

    (Comment): Added commentsto identity factory method design pattern interface

--- FactoryPattern/FactoryPattenConcept/src/main/java/com/technostack/conceptImpl2/factory/DishFactory.java
+++ FactoryPattern/FactoryPattenConcept/src/main/java/com/technostack/conceptImpl2/factory/DishFactory.java
@@ -2,6 +2,9 @@ package com.technostack.conceptImpl2.factory;
 
 import com.technostack.conceptImpl2.Dish;
 
+/**
+ * This is an interface which is following Factory Method Design Pattern.
+ */
 public interface DishFactory {
     Dish createDishObject();
 }
