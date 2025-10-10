4213d04a567da40d1f389841f6b25f5e68123a15
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2025-10-10 23:56:03 +0530

    (comments): comments added for the method

--- FactoryPattern/FactoryPattenConcept/src/main/java/com/technostack/conceptImpl2/factory/DishFactory.java
+++ FactoryPattern/FactoryPattenConcept/src/main/java/com/technostack/conceptImpl2/factory/DishFactory.java
@@ -6,5 +6,6 @@ import com.technostack.conceptImpl2.Dish;
  * This is an interface which is following Factory Method Design Pattern.
  */
 public interface DishFactory {
+    // This method is declared for creating factory objects of concrete types.
     Dish createDishObject();
 }
